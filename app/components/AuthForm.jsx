8b02d643e21ba49ada1277743c1ed092b20f43f5
negar <negar.karimnejad@gmail.com>
2024-01-24 18:58:16 +0330

    create AuthForm component

---
+++ app/components/AuthForm.jsx
@@ -0,0 +1,26 @@
+import { Auth } from "@supabase/auth-ui-react";
+import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
+
+function AuthForm() {
+  const supabase = createClientComponentClient();
+  return (
+    <Auth
+      supabaseClient={supabase}
+      view="magic-link"
+      showLinks={false}
+      providers={[]}
+      redirectTo="http://localhost:3000/auth/callback"
+      appearance={{
+        theme: "dark",
+        button: {
+          className: "bg-white-400 text-gray-900 hover:bg-gray-600",
+        },
+        input: {
+          className: "bg-gray-700 border-gray-600 text-white",
+        },
+      }}
+    />
+  );
+}
+
+export default AuthForm;
