6b1e26790428323fb172fa19cfd3c68e0606629a
negar <negar.karimnejad@gmail.com>
2024-01-24 20:51:50 +0330

    create callback route

--- app/components/AuthForm.jsx
+++ app/components/AuthForm.jsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { Auth } from "@supabase/auth-ui-react";
 import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
 
@@ -6,7 +8,7 @@ function AuthForm() {
   return (
     <Auth
       supabaseClient={supabase}
-      view="magic-link"
+      view="magic_link"
       showLinks={false}
       providers={[]}
       redirectTo="http://localhost:3000/auth/callback"
